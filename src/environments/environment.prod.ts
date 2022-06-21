c276f1584552b6b04c85bde479b04c46124750f9
MoritzLuca <102482650+MoritzLuca@users.noreply.github.com>
2022-06-21 22:41:53 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
     firebaseConfig: {
-	apiKey: "AIzaSyC2x9EZLUGhCRwjOorOtNZ7YyEqXeJxbgM",
-	authDomain: "learnfirebase-8ffe2.firebaseapp.com",
-    databaseURL: "????????????",
-	projectId: "learnfirebase-8ffe2",
-	storageBucket: "learnfirebase-8ffe2.appspot.com",
-	messagingSenderId: "585876253251",
-	appId: "1:585876253251:web:39d00d170956ea165382f6"
+	apiKey: "",
+	authDomain: "",
+  databaseURL: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: ""
   }
 };
 // Import the functions you need from the SDKs you need
